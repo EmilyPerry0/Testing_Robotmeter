92db3e2750dbc00b1c183b25a2cc50ed55af89f4
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-06-30 11:59:32 +0200

    removed the model download step so it must be done locally first.

--- robometer_slurm.sh
+++ robometer_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -J test_robometer
-#SBATCH -t 20:00
+#SBATCH -t 5:00
 #SBATCH --partition=gpu_a100
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
@@ -10,27 +10,15 @@
 
 module load 2025
 module load CUDA/12.9.1
-module load Python/3.13.5-GCCcore-14.3.0
 
-# setup
-pip install uv
-pip install -U "huggingface_hub"
-export HF_TOKEN=$(cat ~/.hf_token)
-
-# move to scratch 
-cd $TMPDIR
-
-git clone https://github.com/robometer/robometer.git
-cd robometer
-
-# Create venv and install
-uv sync
+# move to robometer dr 
+cd $HOME/robometer_setup/robometer
 
 # actually run the thing
 uv run python scripts/example_inference_local.py \
   --model-path robometer/Robometer-4B \
   --video scripts/example_videos/soar_put_green_stick_in_brown_bowl.mp4 \
-  --task "test task description"
+  --task "put green stick in brown bowl"
 
 
 # # setup the output
