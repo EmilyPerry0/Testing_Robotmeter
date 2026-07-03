68d5e36a752ca7615d9d8dd727ffbddd5e6b8b3f
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-07-03 19:30:46 +0200

    added a .sh file to setup the environmen automatically and changed the slurm script to match

--- robometer_slurm.sh
+++ robometer_slurm.sh
@@ -9,16 +9,13 @@
 #SBATCH --output=%x_%j.log
 
 module load 2025
-# module load CUDA/12.9.1
 module load FFmpeg/7.1.1-GCCcore-14.2.0
 
 # move to robometer dr 
-cd $HOME/robometer_setup/robometer
-
-uv sync
+cd $HOME/robometer
 
 # actually run the thing
 uv run python scripts/example_inference_local.py \
-  --model-path robometer/Robometer-4B \
+  --model-path robometer/models/Robometer-4B \
   --video scripts/example_videos/soar_put_green_stick_in_brown_bowl.mp4 \
   --task "put green stick in brown bowl"
