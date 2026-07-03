cdee778ffe8a850161b27d2dd1ad499105c8af96
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-07-03 19:23:05 +0200

    did some changes locally and trying the base setup again

--- robometer_slurm.sh
+++ robometer_slurm.sh
@@ -15,22 +15,10 @@ module load FFmpeg/7.1.1-GCCcore-14.2.0
 # move to robometer dr 
 cd $HOME/robometer_setup/robometer
 
-uv sync --extra robometer
-
-# test
-uv run python -c "from transformers import Qwen3VLModel; print(Qwen3VLModel)"
+uv sync
 
 # actually run the thing
-# uv run python scripts/example_inference_local.py \
-#   --model-path robometer/Robometer-4B \
-#   --video scripts/example_videos/soar_put_green_stick_in_brown_bowl.mp4 \
-#   --task "put green stick in brown bowl"
-
-
-# # setup the output
-# RUN_NAME=$(date +%Y%m%d_%H%M)_${SLURM_JOB_ID}
-# UNIQUE_RUN_DIR="$TMPDIR/$RUN_NAME"
-# mkdir -p "$UNIQUE_RUN_DIR"
-
-# # move the output back to home when done
-# cp -r "$UNIQUE_RUN_DIR" $HOME
+uv run python scripts/example_inference_local.py \
+  --model-path robometer/Robometer-4B \
+  --video scripts/example_videos/soar_put_green_stick_in_brown_bowl.mp4 \
+  --task "put green stick in brown bowl"
