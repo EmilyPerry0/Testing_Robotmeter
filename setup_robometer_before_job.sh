e60d7f1de4eba04fae9453b736337818020f128f
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-07-04 09:46:13 +0200

    changed the model path in the setup file

--- setup_robometer_before_job.sh
+++ setup_robometer_before_job.sh
@@ -5,4 +5,4 @@ git clone https://github.com/robometer/robometer.git
 cd robometer
 uv sync
 hf download robometer/Robometer-4B --local-dir models
-sed -i 's/use_unsloth: *true/use_unsloth: false/' models/Robometer-4B/config.yaml
+sed -i 's/use_unsloth: *true/use_unsloth: false/' models/config.yaml
