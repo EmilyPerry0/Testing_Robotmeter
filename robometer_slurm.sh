e9ad892766aae1f7e7c95bc354c7a1f936aca4d9
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-07-06 14:11:54 +0200

    adding back in model path, but its the default one now

--- robometer_slurm.sh
+++ robometer_slurm.sh
@@ -17,5 +17,6 @@ cd $HOME/robometer
 # actually run the thing
 uv run python scripts/example_inference_local.py \
   --video scripts/example_videos/soar_put_green_stick_in_brown_bowl.mp4 \
+  --model-path robometer/Robometer-4B \
   --task "pick up green stick and put green stick in brown bowl" \
   --fps 5
